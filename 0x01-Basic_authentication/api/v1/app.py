c54680e4d6afabcb276c0daf67c94161563168a4
Femi Mehalayese <megafemclick@gmail.com>
2024-11-12 15:57:13 +0100

    forbidden fruit

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,6 +28,13 @@ def unauthorized_access(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def fordibben_access(error) -> str:
+    """ Forbidden access
+    """
+    return jsonify({"error": "Forbidden"})
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
