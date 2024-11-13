61e5db7a0adb945b2afab60983857c99f7c88b12
Femi Mehalayese <megafemclick@gmail.com>
2024-11-13 18:56:34 +0100

    ee

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,9 @@ class Auth:
         """
             Handle request header
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
