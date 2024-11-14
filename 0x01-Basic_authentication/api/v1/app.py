a854879de29025084b7814cdf28a077a98fddf4e
Femi Mehalayese <megafemclick@gmail.com>
2024-11-14 12:05:39 +0100

    basicauth cls 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,7 +13,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = getenv("AUTH_TYPE", None)
-
+"""
+    check which authentication to use
+"""
 if auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
