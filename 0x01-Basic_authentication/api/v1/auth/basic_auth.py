34704023a5365ed0d3979f25dee0d102b8e735a9
Femi Mehalayese <megafemclick@gmail.com>
2024-11-14 13:40:18 +0100

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,17 @@ class BasicAuth(Auth):
     """
         BasicAuth inherit from Auth
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+             returns the Base64 part of the Authorization header
+             for a Basic Authentication:
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            delt = "Basic "
+            return authorization_header.split(delt, 1)[-1]
