0a33cc0f61ce4af112f5601f5953238b844fcd1f
Femi Mehalayese <megafemclick@gmail.com>
2024-11-18 15:14:33 +0100

    user authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -62,12 +64,14 @@ class BasicAuth(Auth):
         """
             returns the User instance based on his email and password
         """
-        if user_email is None or if not isinstance(user_email, str):
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None or if not isinstance(user_pwd, str):
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        if not search(user_email):
+        user = User.search({'email': user_email})
+        if not user or len(user) <= 0:
             return None
-        if not is_valid_password(user_email) == user_pwd:
+        user = user[0]
+        if not user.is_valid_password(user_pwd):
             return None
-        return User()
+        return user
