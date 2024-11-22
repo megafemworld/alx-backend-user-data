64133767946b1b0cad48a6f092eef590349a6b76
Femi Mehalayese <megafemclick@gmail.com>
2024-11-22 16:10:14 +0100

    db 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """save the user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
