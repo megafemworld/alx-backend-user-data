f4389aa6d8b959baf163298b85f0a209c3ca0169
Femi Mehalayese <megafemclick@gmail.com>
2024-11-23 20:53:05 +0100

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -37,3 +38,14 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        returns the first row found in the users table
+        as filtered by the method’s input arguments.
+        """
+        for key, in kwargs:
+            if not hasattr(User, key):
+                raise InvalidRequestError
+        return self._session.query(User).filter_by(**kwargs).one()
+
