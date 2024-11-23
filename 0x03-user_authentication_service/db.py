9cf4dfed1987d8e40c74f63c29d8baf2e5e47f44
Femi Mehalayese <megafemclick@gmail.com>
2024-11-23 19:04:51 +0100

    db 12

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -13,11 +13,10 @@ from user import Base, User
 class DB:
     """DB class
     """
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
