3648dc36df4e03c7d379d37e5180ac270d50a2cc
Femi Mehalayese <megafemclick@gmail.com>
2025-01-05 18:28:06 +0100

    spelling error checker notice

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,9 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """Filter_datum the message by replacing"""
     for f in fields:
-        message = re.sub(f'{f}=.*?{seperator}',
-                         f'{f}={redaction}{seperator}', message)
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
