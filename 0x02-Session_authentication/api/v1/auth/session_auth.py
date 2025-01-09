489ddbd96698fcc1899ca218670911df5a0d78a8
Femi Mehalayese <megafemclick@gmail.com>
2025-01-09 23:22:28 +0100

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    class SessionAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        SessionAuth inherit from Auth
+    """
+    pass
