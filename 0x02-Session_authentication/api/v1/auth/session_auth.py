be81901f72f7289bcfa2bc35573e3252c7c5829f
Femi Mehalayese <megafemclick@gmail.com>
2025-01-11 20:59:59 +0100

    uudid importted and used

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
     class SessionAuth that inherits from Auth
 """
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
@@ -12,7 +13,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """_summary_
+        """creates a Session ID for a user_id
 
         Args:
             user_id (str, optional): _description_. Defaults to None.
@@ -22,7 +23,7 @@ class SessionAuth(Auth):
         """
         if user_id is None or type(user_id) is not str:
             return None
-        session_id = super().create_session(user_id)
+        session_id = str(uuid4())
         if session_id is None:
             return None
         self.user_id_by_session_id[session_id] = user_id
