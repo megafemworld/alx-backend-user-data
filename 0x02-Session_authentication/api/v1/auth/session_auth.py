86f22b8c590b4e6c14317bdd1380c885638fb4d6
Femi Mehalayese <megafemclick@gmail.com>
2025-01-11 20:46:51 +0100

    create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,4 +9,21 @@ class SessionAuth(Auth):
     """
         SessionAuth inherit from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """_summary_
+
+        Args:
+            user_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
