07e2c9b93634e245675d864b7cc209db90f3c94a
Femi Mehalayese <megafemclick@gmail.com>
2025-01-11 21:11:02 +0100

    User ID for Session ID 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,7 @@ class SessionAuth(Auth):
             return None
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ returns a User ID based on a Session ID
 
